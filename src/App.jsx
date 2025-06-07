97aecb086cd955cc3b70d268d4baac640c4cb3a9
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-06-07 16:53:06 +0100

    adding more

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./style.css";
+import { MovieCard } from "./components/MovieCard";
+
+//conditional rendering
 
 function App() {
-  return <></>;
+  const movieNumber = 1;
+
+  return (
+    <>
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
+      ) : (
+        <MovieCard movie={{ title: "Joes Film", release_date: "2020" }} />
+      )}
+    </>
+  );
 }
 
 export default App;
