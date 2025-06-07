980522666862ff738d4aa54dc0b882f62174b148
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-06-07 16:53:49 +0100

    adding more 2

--- src/assets/react-playground.jsx
+++ src/assets/react-playground.jsx
@@ -19,4 +19,22 @@ function Component({ property }) {
   );
 }
 
-export default App;
+// export default App;
+
+//conditional rendering
+
+function App() {
+  const movieNumber = 1;
+
+  return (
+    <>
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
+      ) : (
+        <MovieCard movie={{ title: "Joes Film", release_date: "2020" }} />
+      )}
+    </>
+  );
+}
+
+// export default App;
